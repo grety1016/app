cebbe3895fccb3c913bb59e306410055b711745a
grety <snl1016@163.com>
2024-07-19 11:51:36 +0800

    u

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -2,26 +2,11 @@
   <div>
     <van-nav-bar title="登录" class="login-nav-bar" />
     <van-form>
-      <van-field
-        v-model="user.userName"
-        left-icon="user"
-        name="userName"
-        label="用户名"
-        placeholder="用户名"
-        :rules="[{ required: true, message: '请输入用户名' }]"
-      />
-      <van-field
-        v-model="user.userPwd"
-        left-icon="lock"
-        type="password"
-        name="userPwd"
-        label="密码"
-        placeholder="密码"
-        :rules="[{ required: true, message: '请输入密码' }]"
-      />
-      <van-button block type="primary" native-type="submit" @click="printMsg">
-        提交
-      </van-button>
+      <van-field v-model="user.userName" left-icon="user" name="userName" label="用户名" placeholder="用户名"
+        :rules="[{ required: true, message: '请输入用户名' }]" />
+      <van-field v-model="user.userPwd" left-icon="lock" type="password" name="userPwd" label="密码" placeholder="密码"
+        :rules="[{ required: true, message: '请输入密码' }]" />
+      <van-button block type="primary" native-type="submit" @click="printMsg">提交</van-button>
     </van-form>
   </div>
 </template>
