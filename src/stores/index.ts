d8cd3ac53d63469f0f2edc81854b83cbe5df67d1
grety <snl1016@163.com>
2024-07-23 16:46:24 +0800

    u

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,9 +1,30 @@
 import { createPinia,defineStore } from 'pinia';
-import   piniaPluginPersistedstate  from 'pinia-plugin-persistedstate'
+import   piniaPluginPersistedstate  from 'pinia-plugin-persistedstate' 
+import { reactive } from 'vue';
 
 export const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate);
 
-export const useStore = defineStore('Index',() =>{
+//定义user类型接口
+export interface IUser {
+    userName: string;
+    userPwd: string;
+  }
 
+export const userLoginStore = defineStore('userLoginStore',() =>{
+    //定义userLoginStore变量
+    let loginUser: IUser = reactive<IUser>({} as IUser);
+
+    //定义userLoginStore函数
+    const setUserData = (data: IUser) =>{
+        loginUser = data;
+    }
+    
+
+},{
+    persist:{
+        key:'userLoginStore',
+        storage:window.localStorage,
+        
+    }
 });
