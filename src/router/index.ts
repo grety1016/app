da1e3632e44e5d1cc5ba047dc6ba8d7bc17aae08
grety <snl1016@163.com>
2024-07-29 14:13:22 +0800

    u

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import {createRouter,createWebHistory} from 'vue-router'
 const LoginView = () => import('@/views/Login/LoginView.vue')
+const FlowForm = () => import('@/views/FlowForm/FlowForm.vue')
 
 const routes = [
   {
     path: '/login', 
     name: 'Login',
     component: LoginView,
+  },
+  {
+    path:'/flowform',
+    name: 'FlowForm',
+    component: FlowForm,
   }
 ];
 
